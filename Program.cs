7c96b74f6f8dda4b923305551ce10558a1f8d424
Mert Topcu <merttopcu.dev@gmail.com>
2024-08-10 17:13:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaListTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ▼ Greeting ▼
+            Console.WriteLine("Welcome to the Patika Plus Gala! Here are the guest list:\n*******************");
+
+            // ▼ Creating generic list ▼
+            List<string> guestList = new List<string>();
+
+            // ▼ Adding 10 random full names to list with 'FakeData' package ▼
+            for (int i = 0; i < 10; i++)
+            {
+                // ▼ FakeData is a 'NuGet packege' in project to create random full names ▼
+                guestList.Add(FakeData.NameData.GetFullName());
+            }
+
+            // ▼ Printing all guest ▼
+            foreach (string guest in guestList)
+            {
+                Console.WriteLine(guest);
+            }
+            Console.WriteLine("*******************");
+        }
+    }
+}
